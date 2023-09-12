8efcfea0465de8ea9733b63fac30d8e209d18984
Nitin Manik <40004661+ntnmnk@users.noreply.github.com>
2023-09-12 14:43:21 +0530

    Time: 53 ms (20.17%), Space: 44.4 MB (65.1%) - LeetHub

--- 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,37 +1,27 @@
 class Solution {
     public int minDeletions(String s) {
-               // Create a HashMap to count the frequency of each character.
-        Map<Character, Integer> frequencyMap = new HashMap<>();
+        Map<Character, Integer> charFrequency = new HashMap<>();
+        Set<Integer> uniqueFrequencies = new HashSet<>();
+        int deletions = 0;
 
-        // Iterate through the characters in the input string 's'.
+        // Count the frequency of each character
         for (char c : s.toCharArray()) {
-            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+            charFrequency.put(c, charFrequency.getOrDefault(c, 0) + 1);
         }
 
-        // Create a max-heap (PriorityQueue) to store character frequencies in decreasing order.
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
-
-        // Populate the max-heap with character frequencies from the map.
-        maxHeap.addAll(frequencyMap.values());
-
-        // Initialize a variable to keep track of the minimum number of deletions needed.
-        int count = 0;
-
-        // Continue as long as there are at least two frequencies in the max-heap.
-        while (maxHeap.size() > 1) {
-            int top = maxHeap.poll(); // Get the character frequency with the highest count.
-
-            // Check if the next character in the max-heap has the same frequency as 'top' (and it's not zero).
-            if (maxHeap.peek() != null && maxHeap.peek() == top && top != 0) {
-                count++; // Increment the deletion count.
-                maxHeap.add(top - 1); // Decrease 'top' frequency by 1 and push it back into the max-heap.
+        // Iterate through the frequencies
+        for (int freq : charFrequency.values()) {
+            while (uniqueFrequencies.contains(freq)) {
+                // Increment deletions and reduce the frequency until it's unique
+                freq--;
+                deletions++;
+            }
+            if (freq > 0) {
+                uniqueFrequencies.add(freq);
             }
         }
 
-        // Return the minimum number of deletions required to make the string "good."
-        return count;
-
-
-   
+        return deletions;
+    
     }
 }
