c3374e42fe7b27788aaa79b1a501fc7ce005a57b
Nitin Manik <40004661+ntnmnk@users.noreply.github.com>
2023-09-19 19:51:42 +0530

    Time: 9 ms (30.67%), Space: 62.4 MB (7.82%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,21 +1,24 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-            int i = 0;
-        while(i < nums.length){
-            if(nums[i] == i+1 || nums[i] <= 0 || nums[i] > nums.length) i++;
-            else if(nums[nums[i]-1] != nums[i]) swap(nums, i, nums[i]-1);
-            else i++;
+          int n = nums.length;
+    Set<Integer> set = new HashSet<>();
+
+    // Step 1: Add all positive integers to the set
+    for (int num : nums) {
+        if (num > 0) {
+            set.add(num);
         }
-        i = 0;
-        while(i < nums.length && nums[i] == i+1) i++;
-        return i+1;
-
-   
-    }
-        private void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 
-}
+    // Step 2: Find the smallest missing positive integer
+    for (int i = 1; i <= n; i++) {
+        if (!set.contains(i)) {
+            return i;
+        }
+    }
+
+    // If all positive integers from 1 to n are present, return n + 1
+    return n + 1;
+
+
+}}
