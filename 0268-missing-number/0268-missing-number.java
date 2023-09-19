7e89bb75569b36514f801bb6381b2146b7790c0f
Nitin Manik <40004661+ntnmnk@users.noreply.github.com>
2023-09-19 20:01:52 +0530

    Time: 1 ms (41.15%), Space: 43.7 MB (89.15%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,22 +1,31 @@
 class Solution {
     public int missingNumber(int[] nums) {
-         int n = nums.length;
+       int n = nums.length;
     
-    // Create a HashMap to store the numbers in the array as keys
-    Map<Integer, Integer> numMap = new HashMap<>();
-    for (int num : nums) {
-        numMap.put(num, 1);
+    // Perform cyclic sort to rearrange numbers to their correct positions
+    for (int i = 0; i < n; i++) {
+        while (nums[i] >= 0 && nums[i] < n && nums[i] != nums[nums[i]]) {
+            swap(nums, i, nums[i]);
+        }
     }
     
-    // Check for the missing number in the range [0, n]
-    for (int i = 0; i <= n; i++) {
-        if (!numMap.containsKey(i)) {
+    // Find the first index where nums[i] != i, that's the missing number
+    for (int i = 0; i < n; i++) {
+        if (nums[i] != i) {
             return i;
         }
     }
     
-    // Return -1 if no missing number is found (this should not happen)
-    return -1;
+    // If all numbers from 0 to n-1 are present, return n
+    return n;
+}
 
-    }
+// Helper method to swap elements in the array
+private void swap(int[] nums, int i, int j) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
+    
 }
