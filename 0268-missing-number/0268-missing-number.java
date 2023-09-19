fa94bada314ea4022a29589c1944f91eac2600fc
Nitin Manik <40004661+ntnmnk@users.noreply.github.com>
2023-09-19 19:59:55 +0530

    Time: 4 ms (27.94%), Space: 44.3 MB (33.85%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,12 +1,22 @@
 class Solution {
     public int missingNumber(int[] nums) {
-      int missing = nums.length; 
-          // XOR all numbers from 0 to n
-    for (int i = 0; i < nums.length; i++) {
-        missing ^= i ^ nums[i];
+         int n = nums.length;
+    
+    // Create a HashMap to store the numbers in the array as keys
+    Map<Integer, Integer> numMap = new HashMap<>();
+    for (int num : nums) {
+        numMap.put(num, 1);
     }
     
-    return missing;
+    // Check for the missing number in the range [0, n]
+    for (int i = 0; i <= n; i++) {
+        if (!numMap.containsKey(i)) {
+            return i;
+        }
+    }
+    
+    // Return -1 if no missing number is found (this should not happen)
+    return -1;
 
     }
 }
