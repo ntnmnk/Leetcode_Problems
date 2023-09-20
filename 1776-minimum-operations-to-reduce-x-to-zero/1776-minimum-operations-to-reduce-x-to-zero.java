542816af87a70985879e9c2e8aeab7c8ccd4ffb7
Nitin Manik <40004661+ntnmnk@users.noreply.github.com>
2023-09-20 11:42:51 +0530

    Time: 5 ms (63.89%), Space: 61.1 MB (31.39%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.java
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.java
@@ -1,37 +1,16 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
-      int n = nums.length;
-    int targetSum = Arrays.stream(nums).sum() - x;
-    
-    if (targetSum == 0) {
-        return n; // The entire array can be removed
-    }
-    
-    Map<Integer, Integer> sumMap = new HashMap<>();
-    sumMap.put(0, -1); // Initialize the map with a sum of 0 at index -1
-    int currentSum = 0;
-    int maxLength = -1; // Initialize to -1 to handle the edge case
-    
-    for (int i = 0; i < n; i++) {
-        currentSum += nums[i];
-        
-        if (sumMap.containsKey(currentSum - targetSum)) {
-            // A subarray with the desired sum is found
-            maxLength = Math.max(maxLength, i - sumMap.get(currentSum - targetSum));
-        }
-        
-        // Store the current sum in the map if not already present
-        if (!sumMap.containsKey(currentSum)) {
-            sumMap.put(currentSum, i);
-        }
-    }
-    
-    if (maxLength == -1) {
-        return -1; // No subarray with the desired sum is found
-    }
-    
-    // Calculate the minimum number of operations to achieve the desired sum
-    return n - maxLength;
+     	int sum = 0;
+	for (int num: nums) sum += num;
 
+	int maxLength = -1, currSum = 0;
+	for (int l=0, r=0; r<nums.length; r++) {
+		currSum += nums[r];
+		while (l <= r && currSum > sum - x) currSum -= nums[l++];
+		if (currSum == sum - x) maxLength = Math.max(maxLength, r-l+1);
+	}
+
+	return maxLength == -1 ? -1 : nums.length - maxLength;
+   
     }
 }
