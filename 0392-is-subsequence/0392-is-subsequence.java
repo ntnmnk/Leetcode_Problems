50a6f82b918cb0491a5a2b53698d2c7915668f57
Nitin Manik <40004661+ntnmnk@users.noreply.github.com>
2023-09-22 20:33:42 +0530

    Time: 6 ms (5%), Space: 40.8 MB (18.89%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,21 +1,27 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-            // Base case: if the s string is empty...
-        if(s.length() == 0)
-            return true;
-        // Initialize pointers for both strings
-        int i = 0;
-        int j = 0;
-        // We can iterate until either of them becomes zero...
-        while(i < s.length() && j < t.length()){
-            // Compare characters, increment i pointer...
-            if(s.charAt(i) == t.charAt(j)){
-                i++;
-            }j++;
-            // If the pointer is equal to the size of s, the match is found...
-            if(i == s.length()) return true;
+               boolean[][] dp = new boolean[s.length()+1][t.length()+1];
+        
+        //Empty string is subsequence of empty string
+        dp[0][0] = true;
+        
+        for(int i=1; i<=s.length(); i++) {
+            dp[i][0] = false; 
         }
-        return false;       // Otherwise return false...
-  
-
+        
+        for(int j=1; j<=t.length(); j++) {
+            dp[0][j] = true;
+        }
+        
+        for(int i=1; i<=s.length(); i++) {
+            for(int j=1; j<=t.length(); j++) {
+                if(s.charAt(i-1) == t.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = dp[i][j-1];
+                }  
+            }
+        }
+        
+        return dp[s.length()][t.length()];
 }}
