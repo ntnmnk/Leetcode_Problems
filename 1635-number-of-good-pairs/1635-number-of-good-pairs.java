bbdf94d7b36c6170704d7d26708a70794108bd76
Nitin Manik <40004661+ntnmnk@users.noreply.github.com>
2023-10-03 10:56:28 +0530

    Time: 0 ms (100%), Space: 39.5 MB (93.04%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.java
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.java
@@ -1,15 +1,12 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
-        int[] count = new int[101]; // We assume the range of integers in nums is from 1 to 100
-    int goodPairs = 0;
-
-    for (int num : nums) {
-        // Increment the count of the current number and add the current count to the goodPairs
-        goodPairs += count[num];
-        count[num]++;
+    Map<Integer,Integer> map =new HashMap<>();
+    int goodpairs=0;
+    for(int num:nums)    {
+        int count=map.getOrDefault(num,0);
+        goodpairs+=count;
+        map.put(num,count+1);
     }
-
-    return goodPairs;
-    
+    return goodpairs;
     }
 }
