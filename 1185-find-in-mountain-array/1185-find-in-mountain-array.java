998659777ad29dc984867be59eb930fc93201b87
Nitin Manik <40004661+ntnmnk@users.noreply.github.com>
2023-10-12 18:18:10 +0530

    Time: 0 ms (100%), Space: 43.8 MB (8.29%) - LeetHub

--- 1185-find-in-mountain-array/1185-find-in-mountain-array.java
+++ 1185-find-in-mountain-array/1185-find-in-mountain-array.java
@@ -9,15 +9,24 @@
  
 class Solution {
     public int findInMountainArray(int target, MountainArray mountainArr) {
-             // Save the length of the mountain array
+               // Save the length of the mountain array
         int length = mountainArr.length();
 
         // 1. Find the index of the peak element
         int low = 1;
         int high = length - 2;
         while (low != high) {
-            int testIndex = (low + high) / 2;
-            if (mountainArr.get(testIndex) < mountainArr.get(testIndex + 1)) {
+            int testIndex = (low + high) >> 1;
+            int curr = mountainArr.get(testIndex);
+            int next = mountainArr.get(testIndex + 1);
+            
+            if (curr < next) {
+                if (curr == target) {
+                    return testIndex;
+                }
+                if (next == target) {
+                    return testIndex + 1;
+                }
                 low = testIndex + 1;
             } else {
                 high = testIndex;
@@ -26,38 +35,40 @@ class Solution {
         int peakIndex = low;
 
         // 2. Search in the strictly increasing part of the array
+        // If found, will be returned in the loop itself.
         low = 0;
         high = peakIndex;
-        while (low != high) {
-            int testIndex = (low + high) / 2;
-            if (mountainArr.get(testIndex) < target) {
+        while (low <= high) {
+            int testIndex = (low + high) >> 1;
+            int curr = mountainArr.get(testIndex);
+                
+            if (curr == target) {
+                return testIndex;
+            } else if (curr < target) {
                 low = testIndex + 1;
             } else {
-                high = testIndex;
+                high = testIndex - 1;
             }
-        }
-        // Check if the target is present in the strictly increasing part
-        if (mountainArr.get(low) == target) {
-            return low;
         }
 
-        // 3. Otherwise, search in the strictly decreasing part
+        // 3. Search in the strictly decreasing part of the array
+        // If found, will be returned in the loop itself.
         low = peakIndex + 1;
         high = length - 1;
-        while (low != high) {
-            int testIndex = (low + high) / 2;
-            if (mountainArr.get(testIndex) > target) {
+        while (low <= high) {
+            int testIndex = (low + high) >> 1;
+            int curr = mountainArr.get(testIndex);
+                
+            if (curr == target) {
+                return testIndex;
+            } else if (curr > target) {
                 low = testIndex + 1;
             } else {
-                high = testIndex;
+                high = testIndex - 1;
             }
-        }
-        // Check if the target is present in the strictly decreasing part
-        if (mountainArr.get(low) == target) {
-            return low;
         }
 
         // Target is not present in the mountain array
-        return -1;   
+        return -1;
     }
 }
