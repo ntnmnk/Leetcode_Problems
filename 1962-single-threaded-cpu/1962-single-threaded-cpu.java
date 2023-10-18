94e7477246d61b6dea4ce13ccc70fc70577242c9
Nitin Manik <40004661+ntnmnk@users.noreply.github.com>
2023-10-18 14:17:34 +0530

    Time: 152 ms (20.6%), Space: 75.4 MB (70.03%) - LeetHub

--- 1962-single-threaded-cpu/1962-single-threaded-cpu.java
+++ 1962-single-threaded-cpu/1962-single-threaded-cpu.java
@@ -1,32 +1,26 @@
 class Solution {
     public int[] getOrder(int[][] tasks) {
-            int n = tasks.length;
-        int[] ans = new int[n];
-        int[][] extTasks = new int[n][3];
-        for(int i = 0; i < n; i++) {
-            extTasks[i][0] = i;
-            extTasks[i][1] = tasks[i][0];
-            extTasks[i][2] = tasks[i][1];
+            PriorityQueue<Task> taskQueue = new PriorityQueue<>((x,y)-> x.enqueueTime-y.enqueueTime), availableQueue = new PriorityQueue<>((x,y)->x.processingTime!=y.processingTime ? x.processingTime-y.processingTime : x.index-y.index);
+        int[] result = new int[tasks.length];
+        for(int i=0;i<tasks.length;i++) taskQueue.offer(new Task(i, tasks[i][0], tasks[i][1]));
+        int index=0, currentTime = taskQueue.peek().enqueueTime;
+        while(!taskQueue.isEmpty() || !availableQueue.isEmpty()) {
+            while(!taskQueue.isEmpty() && taskQueue.peek().enqueueTime <= currentTime)
+                availableQueue.offer(taskQueue.poll());
+            if(!availableQueue.isEmpty()) {
+                currentTime = currentTime + availableQueue.peek().processingTime;
+                result[index++] = availableQueue.poll().index;
+            } else currentTime = taskQueue.peek().enqueueTime;
         }
-        Arrays.sort(extTasks, (a,b)->a[1] - b[1]);
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> a[2] == b[2] ? a[0] - b[0] : a[2] - b[2]);
-        int time = 0;
-        int ai = 0;
-        int ti = 0;
-        while(ai < n) {
-            while(ti < n && extTasks[ti][1] <= time) {
-                pq.offer(extTasks[ti++]);
-                
-            }
-            if(pq.isEmpty()) {
-                time = extTasks[ti][1];
-                continue;
-            }
-            int[] bestFit = pq.poll();
-            ans[ai++] = bestFit[0];
-            time += bestFit[2];
-        }
-        return ans;
-
+        return result;
+    }
 }
-}
+class Task {
+    int index,enqueueTime,processingTime;
+    public Task(int index, int enqueueTime, int processingTime) {
+        this.index = index;
+        this.enqueueTime = enqueueTime;
+        this.processingTime = processingTime;
+    }
+    
+    }
