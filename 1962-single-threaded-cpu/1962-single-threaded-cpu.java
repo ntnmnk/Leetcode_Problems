57337d98bd50a9c069e41dc3d75e305014392b4e
Nitin Manik <40004661+ntnmnk@users.noreply.github.com>
2023-10-18 14:28:33 +0530

    Time: 96 ms (87.78%), Space: 75.2 MB (79.97%) - LeetHub

--- 1962-single-threaded-cpu/1962-single-threaded-cpu.java
+++ 1962-single-threaded-cpu/1962-single-threaded-cpu.java
@@ -1,78 +1,53 @@
 class Solution {
     public int[] getOrder(int[][] tasks) {
-              // Create a comparator for tasks, first by processing time, then by index
-        Comparator<Task> processingTimeThenIndex = Comparator
-            .comparingInt(Task::getProcessingTime)
-            .thenComparingInt(Task::getIndex);
+            int n = tasks.length;
+        Task [] arr = new Task[n];
+        for(int i = 0 ;i<n;i++){
+            arr[i] = new Task(i, tasks[i][0],tasks[i][1]);
+        }
+
+        Arrays.sort(arr, (a,b)->{
+            return Integer.compare(a.enqueueTime,b.enqueueTime);
+        });
+
+        PriorityQueue<Task> p = new PriorityQueue<>((a,b)->{
+            if(a.processingTime == b.processingTime){
+                return Integer.compare(a.idx,b.idx);
+            }
+            return Integer.compare(a.processingTime,b.processingTime);
+        });
         
-        // Use two priority queues for task management
-        // allTasks contains all tasks and is sorted by enqueue time, processing time, and index
-        Queue<Task> allTasks = new PriorityQueue<>(
-            Comparator.comparingInt(Task::getEnqueueTime).thenComparing(processingTimeThenIndex)
-        );
-        // availableTasks contains currently available tasks and is sorted by processing time and index
-        Queue<Task> availableTasks = new PriorityQueue<>(processingTimeThenIndex);
+        int[] ans = new int[n];
+        int ansIdx = 0;
+        int taskIdx = 0;
+        int curTime= 0;
 
-        // Add tasks to the allTasks queue
-        for (int i = 0; i < tasks.length; i++) {
-            int[] taskInfo = tasks[i];
-            Task task = new Task(taskInfo[0], taskInfo[1], i);
-            allTasks.offer(task);
-        }
-
-        int virtualTime = 0;
-        int orderCounter = 0;
-        int[] order = new int[tasks.length];
-
-        while (!allTasks.isEmpty() || !availableTasks.isEmpty()) {
-            Task nextTask;
-            if (availableTasks.isEmpty()) {
-                // If there are no available tasks, pick the next task from allTasks
-                nextTask = allTasks.poll();
-                virtualTime = nextTask.getEnqueueTime();
-            } else {
-                // Pick the next available task
-                nextTask = availableTasks.poll();
+        while(ansIdx < n){
+            while(taskIdx < n && arr[taskIdx].enqueueTime <= curTime){
+                p.offer(arr[taskIdx++]);
             }
-            order[orderCounter++] = nextTask.getIndex();
-            virtualTime += nextTask.getProcessingTime();
-
-            // Add tasks that become available to the availableTasks queue
-            while (!allTasks.isEmpty() && allTasks.peek().getEnqueueTime() <= virtualTime) {
-                availableTasks.offer(allTasks.poll());
+            if(p.isEmpty()){
+                curTime = arr[taskIdx].enqueueTime;
+            }else{
+                curTime += p.peek().processingTime;
+                ans[ansIdx++] = p.poll().idx;
             }
         }
-
-        return order;
-
+        return ans;       
     }
-}
+    
+    class Task {
+        int idx;
+        int enqueueTime;
+        int processingTime;
 
-class Task {
-    final int enqueueTime;
-    final int processingTime;
-    final int index;
-
-    public Task(
-            final int enqueueTime,
-            final int processingTime,
-            final int index) {
-        this.enqueueTime = enqueueTime;
-        this.processingTime = processingTime;
-        this.index = index;
+        Task(int idx , int en , int pro){
+            this.idx = idx;
+            this.enqueueTime = en;
+            this.processingTime = pro;
+        }
     }
 
-    public int getEnqueueTime() {
-        return enqueueTime;
-    }
-
-    public int getProcessingTime() {
-        return processingTime;
-    }
-
-    public int getIndex() {
-        return index;
-    }
    
     }
 
