3dbb9f6aba9ab53afb47c2b6656293f79b2fd71a
Nitin Manik <40004661+ntnmnk@users.noreply.github.com>
2023-10-25 12:14:07 +0530

    Time: 155 ms (77.78%), Space: 55.1 MB (73.93%) - LeetHub

--- 1830-count-good-meals/1830-count-good-meals.java
+++ 1830-count-good-meals/1830-count-good-meals.java
@@ -1,26 +1,13 @@
 class Solution {
     public int countPairs(int[] deliciousness) {
-            TreeMap<Integer, Integer> cnt = new TreeMap<>();
+              Map<Integer, Integer> cnt = new HashMap<>();
+        long ans = 0;
         for (int d : deliciousness) {
+            for (int power = 1, i = 0; i < 22; ++i, power <<= 1) {
+                ans += cnt.getOrDefault(power - d, 0);        
+            }
             cnt.put(d, 1 + cnt.getOrDefault(d, 0));
         }
-        long ans = 0;
-        while (!cnt.isEmpty()) {
-            var e = cnt.pollLastEntry();
-            int key = e.getKey(), v = e.getValue(), p = 1;
-            while (p <= key) {
-                p <<= 1;
-            }
-            if (Integer.bitCount(key) == 1) {
-                ans += 1L * v * cnt.getOrDefault(0, 0);    
-                ans += (v - 1L) * v / 2;   
-            }else if (cnt.containsKey(p - key)) {
-                ans += 1L * v * cnt.get(p - key);
-            }
-        }
-        return (int)(ans % 1_000_000_007);         
-      
-
-    
+        return (int)(ans % 1_000_000_007);             
     }
 }
