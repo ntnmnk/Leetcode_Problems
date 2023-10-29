1307133a55bd1b18ead47a525bd133faf902ccbe
Nitin Manik <40004661+ntnmnk@users.noreply.github.com>
2023-10-29 19:11:50 +0530

    Time: 0 ms (100%), Space: 39.4 MB (22.73%) - LeetHub

--- 0458-poor-pigs/0458-poor-pigs.java
+++ 0458-poor-pigs/0458-poor-pigs.java
@@ -1,14 +1,15 @@
 class Solution {
     public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-             int max_time = minutesToTest / minutesToDie + 1;
-        // Initialize the required minimum number of pigs...
-        int req_pigs = 0;
-        // To find the minimum number of pigs, find the minimum req_pigs such that Math.pow(max_time, req_pigs) >= buckets...
-        while (Math.pow(max_time, req_pigs) < buckets)
-            // Increment until it will be greater or equals to bucket...
-            ++req_pigs;
-        // Return the required minimum number of pigs...
-        return req_pigs;
+          int test_val = minutesToTest/minutesToDie;
+        int count=0;
+        
+        while( Math.pow(test_val+1,count) < buckets ){
+            count++;
+        }
+        
+        return count;
+    
+
 
     }
 }
