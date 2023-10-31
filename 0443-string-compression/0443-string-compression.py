0c860df21a554115e02e264f6b5615d5475377a5
Nitin Manik <40004661+ntnmnk@users.noreply.github.com>
2023-11-01 02:00:15 +0530

    Time: 59 ms (88.46%), Space: 16.5 MB (24.39%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,23 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        stack=[]
+        s=""
+        for i in range(len(chars)):
+            if len(stack)==0 or stack[-1]==chars[i]:
+                stack.append(chars[i])
+            else:
+                s+=stack[-1]
+                if len(stack)!=1:
+                    s+=str(len(stack))
+                stack.clear()
+                stack.append(chars[i])
+        s+=stack[-1]
+        if len(stack)!=1:
+            s+=str(len(stack))
+        chars.clear()
+        for i in s:
+            chars.append((i))
+        return len(s)
+
+        
+        
