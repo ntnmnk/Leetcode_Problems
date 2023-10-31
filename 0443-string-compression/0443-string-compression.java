2d2048abe1c472d2fa2fb48e752863b8c0727356
Nitin Manik <40004661+ntnmnk@users.noreply.github.com>
2023-10-31 12:05:45 +0530

    Time: 1 ms (99.51%), Space: 42.6 MB (88.49%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,29 +1,29 @@
 class Solution {
     public int compress(char[] chars) {
-         if (chars == null || chars.length <= 1) {
-        return chars.length;
-    }
+         int indexAns = 0;
+    int index = 0;
 
-    int writeIndex = 0;
-    int anchor = 0;
-    
-    for (int readIndex = 0; readIndex < chars.length; readIndex++) {
-        if (readIndex + 1 == chars.length || chars[readIndex] != chars[readIndex + 1]) {
-            chars[writeIndex++] = chars[anchor];
-            if (readIndex > anchor) {
-                int count = readIndex - anchor + 1;
-                if (count > 1) {
-                    String countStr = String.valueOf(count);
-                    for (char countChar : countStr.toCharArray()) {
-                        chars[writeIndex++] = countChar;
-                    }
-                }
+    while (index < chars.length) {
+        char currentChar = chars[index];
+        int count = 0;
+
+        while (index < chars.length && chars[index] == currentChar) {
+            index++;
+            count++;
+        }
+
+        chars[indexAns++] = currentChar;
+
+        if (count > 1) {
+            String countStr = String.valueOf(count);
+            for (char c : countStr.toCharArray()) {
+                chars[indexAns++] = c;
             }
-            anchor = readIndex + 1;
         }
     }
 
-    return writeIndex;
+    return indexAns;
+
    
     }
 }
