8839c55caf7d1675ca2402506d94b8e721e4669f
Nitin Manik <40004661+ntnmnk@users.noreply.github.com>
2023-11-01 02:09:21 +0530

    Time: 1 ms (99.49%), Space: 43.3 MB (26.73%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,38 +1,26 @@
 class Solution {
     public int compress(char[] chars) {
-    if (chars == null || chars.length == 0) {
-        return 0;
-    }
+         int write = 0; // Pointer for writing compressed characters
 
-    // Create a hashmap to count character occurrences
-    HashMap<Character, Integer> charCount = new HashMap<>();
-
-    int n = chars.length;
-    int write = 0; // Pointer for writing compressed characters
-
-    for (int read = 0; read < n; read++) {
+    for (int read = 0; read < chars.length; ) {
+        int count = 0;
         char currentChar = chars[read];
-        
-        // Update the character count in the hashmap
-        charCount.put(currentChar, charCount.getOrDefault(currentChar, 0) + 1);
 
-        if (read == n - 1 || chars[read] != chars[read + 1]) {
-            chars[write++] = currentChar; // Write the character
+        while (read < chars.length && chars[read] == currentChar) {
+            read++;
+            count++;
+        }
 
-            int count = charCount.get(currentChar);
-            if (count > 1) {
-                char[] countChars = Integer.toString(count).toCharArray();
-                for (char c : countChars) {
-                    chars[write++] = c;
-                }
+        chars[write++] = currentChar; // Write the character
+
+        if (count > 1) {
+            for (char c : Integer.toString(count).toCharArray()) {
+                chars[write++] = c;
             }
-
-            // Clear the character count in the hashmap
-            charCount.remove(currentChar);
         }
     }
 
     return write;
-        
+   
     }
 }
